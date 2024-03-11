0f0d3044a39bc1369360a4d939c59468914c4407
mosoveru <mosoveru@gmail.com>
2024-03-11 08:30:32 +0300

    Realised Functionality of Creating Cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+function createCard(name, link, remove) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", remove);
+  document.querySelector(".places__list").append(cardElement);
+}
+
+function removeCallback(event) {
+  const eventTarget = event.target;
+  const deleteItem = eventTarget.closest("li");
+  deleteItem.remove();
+}
+const addButton = document.querySelector(".profile__add-button");
+
+addButton.addEventListener("click", function () {
+  initialCards.forEach(function (item) {
+    createCard(item.name, item.link, removeCallback);
+  });
+});
+
+initialCards.forEach(function (item) {
+  createCard(item.name, item.link, removeCallback);
+});
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
